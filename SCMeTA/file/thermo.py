9a120561086caa53b6e76599d4a173e93baaed5b
EstrellaXD <estrellaxd05@gmail.com>
2025-09-15 19:38:21 +0200

    fix the res problem

--- SCMeTA/file/thermo.py
+++ SCMeTA/file/thermo.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.signal import find_peaks
 
 from .format import SCData
 from . import RawFileReader
@@ -11,9 +12,12 @@ def load_txt(path):
     return raw
 
 
-def load_thermo(path, include_ms2=False):
+def load_thermo(path, include_ms2=False, filter_resolution: bool = True) -> pd.DataFrame:
     reader = RawFileReader(file_path=path)
     data = reader.to_dataframe(include_ms2=include_ms2, filter_threshold=10)
+    if filter_resolution:
+        peaks, _ = find_peaks(data["Intensity"], distance=16)
+        data = data.iloc[peaks]
     data.set_index("Scan", inplace=True)
     data = data.drop(columns=["RetentionTime"])
 
